e4f6a1d8b3c825a8b9c9378677d8780459d95d44
hgidha <hgidha@ucsc.edu>
2024-05-19 19:48:40 -0700

    Added graphql calls to APIService to VendorApp

--- VendorApp/src/graphql/book/service.ts
+++ VendorApp/src/graphql/book/service.ts
@@ -9,12 +9,34 @@
 #######################################################################
 */
 
-import type { Book } from './schema'
+import type { Key } from './schema'
 
-export class BookService{
-  async all(): Promise<Book[]> {
-    const res = await fetch('http://localhost:3012/api/v0/book')
+export class ApikeyService{
+  async all(): Promise<Key[]> {
+    const res = await fetch('http://localhost:3013/api/v0/vendor/api')
     return res.json()
   }
+
+  async one(vendorId: string): Promise<Key[]> {
+    const res = await fetch(`http://localhost:3013/api/v0/vendor/api?vendorid=${vendorId}`)
+    return res.json()
+  }
+
+  async create(vendorId: string): Promise<string | undefined> {
+    const res = await fetch('http://localhost:3013/api/v0/vendor/api', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ vendorid: vendorId }),
+    })
+
+    if (!res.ok) {
+      throw new Error(`Error: ${res.status}`);
+    }
+
+    return res.json()
+  }
+  
 }
 
