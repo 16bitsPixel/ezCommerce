3b1e36ebe410605463da7b764c06b2c8872bbc09
Oliver <oliver.lokhandwala@gmail.com>
2024-05-23 19:11:50 -0700

    added topbar and bottombar to success page

--- ShopperApp/src/pages/success.tsx
+++ ShopperApp/src/pages/success.tsx
@@ -1,5 +1,38 @@
+import React from 'react';
+import Head from 'next/head'
+import { Fragment } from 'react'
+import type { GetServerSideProps } from 'next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+
+import { Cart } from '@/views/Cart';
+import { TopBar } from '@/views/components/TopBar';
+import { BottomBar } from '@/views/components/BottomBar';
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  let language = 'en'
+  if (context.locale) {
+    language = context.locale
+  }
+  return {
+    props: {
+      ...(await serverSideTranslations(language, [
+        'common',
+      ])),
+    },
+  };
+};
+
 export default function Success() {
     return (
-      <h1>Successfully bought!</h1>
+        <Fragment>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <TopBar/>
+            <h1 className='success'>Thank you, your order has been placed.</h1>
+        <BottomBar/>
+
+      </Fragment>
     )
-  }
+}
