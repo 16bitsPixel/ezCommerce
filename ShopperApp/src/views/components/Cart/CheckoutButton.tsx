46dbb8ea3a55db231b0d75a429cabb844a486645
Oliver <oliver.lokhandwala@gmail.com>
2024-05-25 18:03:35 -0700

    if user is not logged in, reroute it to login page when they click checkout

--- ShopperApp/src/views/components/Cart/CheckoutButton.tsx
+++ ShopperApp/src/views/components/Cart/CheckoutButton.tsx
@@ -3,117 +3,81 @@ import React, { useContext } from "react";
 import { useTranslation } from 'next-i18next';
 import { LoginContext } from "@/context/Login";
 import { Product } from "@/graphql/product/schema";
+import { useRouter } from 'next/router';
+
 export function CheckoutButton (){
 
   const {t} = useTranslation('common')
   const {products, setCart, setProducts} = React.useContext(ProductContext)
-      
-//   const handleSubmit = async (event: React.FormEvent) => {
-//     event.preventDefault();
-//     await fetch('/api/checkout_sessions', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify(products),
-//     })
-//       .then(response => {
-//         if (!response.ok) {
-//           return response.text().then(errorText => {
-//             console.error('Error response from server:', errorText);
-//             throw new Error('Network response was not ok');
-//           });
-//         }
-//         return response.json();
-//       })
-//       .then(data => {
-//         if (data.url) {
-//           window.location.href = data.url;
-//         }
-//       })
-//       .then(() => {
-//         await fetch('http://localhost:3015/api/v0/order', {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             }
-//         })
-//       })
-//       .then(() => {
-//         setCart([]);
-//         setProducts([])
-//         localStorage.removeItem('cart')
-//       })
-//       .catch(error => {
-//         console.error('Fetch error:', error);
-//       });
-//   };
-const { id: account_id } = useContext(LoginContext);
+  const router = useRouter();
+  const { id: account_id } = useContext(LoginContext);
 
-const handleSubmit = async (event: React.FormEvent) => {
+  const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
-    try {
-      const response = await fetch('/api/checkout_sessions', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(products),
-      });
-  
-      if (!response.ok) {
-        const errorText = await response.text();
-        console.error('Error response from server:', errorText);
-        throw new Error('Network response was not ok');
-      }
-  
-      const data = await response.json();
-  
-      if (data.url) {
-        window.location.href = data.url;
-      }
-
-      
-      const productMap = new Map<string, number>();
-      for (const product of products) {
-        const productId = (product as Product).id;
-        const quantity = 1;
-        if (productMap.has(productId)) {
-          productMap.set(productId, productMap.get(productId)! + quantity);
-        } else {
-          productMap.set(productId, quantity);
+    if (account_id.length > 0) {
+      try {
+        const response = await fetch('/api/checkout_sessions', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(products),
+        });
+        
+        if (!response.ok) {
+          const errorText = await response.text();
+          console.error('Error response from server:', errorText);
+          throw new Error('Network response was not ok');
         }
+        
+        const data = await response.json();
+        
+        if (data.url) {
+          window.location.href = data.url;
+        }
+    
+            
+        const productMap = new Map<string, number>();
+        for (const product of products) {
+          const productId = (product as Product).id;
+          const quantity = 1;
+          if (productMap.has(productId)) {
+            productMap.set(productId, productMap.get(productId)! + quantity);
+          } else {
+            productMap.set(productId, quantity);
+          }
+        }
+        const inputOrder = {
+          account_id,
+          product_id: Array.from(productMap.keys()),
+          quantities: Array.from(productMap.values()),
+        };
+    
+        console.log("input Order: ", inputOrder)
+    
+        
+        const orderResponse = await fetch('http://localhost:3015/api/v0/order', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(inputOrder)
+        });
+        
+        if (!orderResponse.ok) {
+          const errorText = await orderResponse.text();
+          console.error('Error response from server:', errorText);
+          throw new Error('Network response was not ok');
+        }
+        
+        setCart([]);
+        setProducts([]);
+        localStorage.removeItem('cart');
+      } catch (error) {
+        console.error('Fetch error:', error);
       }
-  
-    //   // Create InputOrder object
-      const inputOrder = {
-        account_id,
-        product_id: Array.from(productMap.keys()),
-        quantities: Array.from(productMap.values()),
-      };
-
-      console.log("input Order: ", inputOrder)
-
-  
-      const orderResponse = await fetch('http://localhost:3015/api/v0/order', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(inputOrder)
-      });
-  
-      if (!orderResponse.ok) {
-        const errorText = await orderResponse.text();
-        console.error('Error response from server:', errorText);
-        throw new Error('Network response was not ok');
-      }
-  
-      setCart([]);
-      setProducts([]);
-      localStorage.removeItem('cart');
-    } catch (error) {
-      console.error('Fetch error:', error);
+    } else {
+      router.push('/login');
     }
   };
   
