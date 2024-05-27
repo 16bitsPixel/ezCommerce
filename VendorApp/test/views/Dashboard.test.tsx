fd35172bd49f2f200b722409339a2899c4d4dfdd
hgidha <hgidha@ucsc.edu>
2024-05-26 22:57:36 -0700

    Dashboard test for VendorApp done

--- VendorApp/test/views/Dashboard.test.tsx
+++ VendorApp/test/views/Dashboard.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import { setupServer } from 'msw/node';
 import Dashboard from '@/views/components/Dashboard';
 import { LoginContext } from '@/context/Login';
 import '@testing-library/jest-dom'; // For custom matchers
+
+jest.mock('next/router', () => require('next-router-mock'));
 
 const server = setupServer();
 
@@ -11,6 +13,43 @@ beforeAll(() => server.listen());
 beforeEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
+const mockSetAccessToken = jest.fn();
+const mockSetUserName = jest.fn();
+const mockSetUserId = jest.fn();
+const mocksetView = jest.fn();
+
+const mockLoginContext = {
+  userName: 'abcd',
+  accessToken: '1234',
+  userId: '5678',
+  setAccessToken: mockSetAccessToken,
+  setUserName: mockSetUserName,
+  setUserId: mockSetUserId,
+  view: 'Login',
+  setView: mocksetView,
+};
+
 it('Renders Dashboard', () => {
     render(<Dashboard/>)
 })
+
+it('clicks logout button', async () => {
+    global.fetch = jest.fn(() =>
+        Promise.resolve({
+          json: () => Promise.resolve({ errors: "N/A" }),
+        })
+    ) as jest.Mock;
+    window.alert = jest.fn()
+    render(
+      <LoginContext.Provider value={mockLoginContext}>
+        <Dashboard />
+      </LoginContext.Provider>
+    );
+    fireEvent.click(screen.getByText("Logout"))
+    await waitFor(() => {
+      expect(mockSetAccessToken).toHaveBeenCalledWith('');
+      expect(mockSetUserName).toHaveBeenCalledWith('');
+      expect(mockSetUserId).toHaveBeenCalledWith('');
+      expect(mocksetView).toHaveBeenCalledWith('Login');
+    });
+});
