39a97ad978edcd45a3bc984019993a1a39dda058
hgidha <hgidha@ucsc.edu>
2024-05-27 01:02:07 -0700

    VendorKeys test for VendorApp done

--- VendorApp/test/views/VendorKeys.test.tsx
+++ VendorApp/test/views/VendorKeys.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import { setupServer } from 'msw/node';
 import { VendorKeys } from '@/views/components/VendorKeys';
-import { LoginContext } from '@/context/Login';
+import { LoginContext, LoginProvider } from '@/context/Login';
 import '@testing-library/jest-dom'; // For custom matchers
 
 jest.mock('next/router', () => require('next-router-mock'));
@@ -28,3 +28,86 @@ const mockLoginContext = {
   view: 'Login',
   setView: mocksetView,
 };
+
+it('renders', () => {
+  render(
+    <LoginProvider>
+      <VendorKeys />
+    </LoginProvider>
+  );
+  expect(screen.getByRole('button', { name: 'Generate New Key' }))
+});
+
+it('renders keytable', async () => {
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      json: () => Promise.resolve({ data: { vendorkeys: [{ id: '1', key: '123' }, { id: '2', key: '456' }, { id: '3', key: '789' }] } }),
+    })
+  ) as jest.Mock;
+
+  render(
+    <LoginContext.Provider value={mockLoginContext}>
+      <VendorKeys />
+    </LoginContext.Provider>
+  );
+
+  await waitFor(() => {
+    expect(screen.getByText('123')).toBeInTheDocument();
+    expect(screen.getByText('456')).toBeInTheDocument();
+    expect(screen.getByText('789')).toBeInTheDocument();
+  });
+});
+
+it('catches error', async () => {
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      json: () => Promise.resolve({ errors: { vendorkeys: [{ id: '1', key: '123' }, { id: '2', key: '456' }, { id: '3', key: '789' }] } }),
+    })
+  ) as jest.Mock;
+  window.alert = jest.fn()
+  render(
+    <LoginContext.Provider value={mockLoginContext}>
+      <VendorKeys />
+    </LoginContext.Provider>
+  );
+
+  await waitFor(() => {
+    expect(window.alert).toHaveBeenCalled()
+  });
+});
+
+it('creates a new key on button click', async () => {
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      json: () => Promise.resolve({ data: { createKey: { id: 'new-key-id', key: 'new-key-value' } } }),
+    })
+  ) as jest.Mock;
+  render(
+    <LoginContext.Provider value={mockLoginContext}>
+      <VendorKeys />
+    </LoginContext.Provider>
+  );
+  fireEvent.click(screen.getByRole('button', { name: 'Generate New Key' }));
+
+  await waitFor(() => {
+    expect(screen.getByText('new-key-value')).toBeInTheDocument();
+  });
+});
+
+it('catches key gen error', async () => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve({ errors: { createKey: { id: 'new-key-id', key: 'new-key-value' } } }),
+      })
+    ) as jest.Mock;
+    render(
+      <LoginContext.Provider value={mockLoginContext}>
+        <VendorKeys />
+      </LoginContext.Provider>
+    );
+    fireEvent.click(screen.getByRole('button', { name: 'Generate New Key' }));
+  
+    await waitFor(() => {
+      expect(window.alert).toHaveBeenCalled()
+    });
+  });
