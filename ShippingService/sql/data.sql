b5ce746f9a2c1ad4aba2dbfc32e1cb6b362da2d5
Josue Martinez <joarmart@ucsc.edu>
2024-06-07 00:44:22 -0700

    No more shipping service

--- ShippingService/sql/data.sql
+++
@@ -1,6 +0,0 @@
-DELETE FROM vendor;
-DELETE FROM account;
-INSERT INTO account(data) VALUES (jsonb_build_object('email','molly@books.com','name','Molly Member','pwhash',crypt('mollymember','87'),'role','member'));
-INSERT INTO account(data) VALUES (jsonb_build_object('email','anna@books.com','name','Anna Admin','pwhash',crypt('annaadmin','87'),'role','admin'));
-INSERT INTO account(id, data) VALUES ('fa14fb7e-2a1d-41d5-8985-30568dc8a7a9', jsonb_build_object('email','vin@vendor.com','name','Vin Vendor','pwhash',crypt('vinvendor','87'),'role','vendor'));
-INSERT INTO vendor(vendor_id,verified ) VALUES ('fa14fb7e-2a1d-41d5-8985-30568dc8a7a9','true');
--- ShippingService/sql/schema.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE IF EXISTS vendor;
-DROP TABLE IF EXISTS account;
-CREATE TABLE account(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), data jsonb);
-CREATE TABLE vendor(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), vendor_id UUID REFERENCES account(id), verified boolean DEFAULT false);
