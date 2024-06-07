b7872549ed9feeaaf19dab14a27c5e3543fe98e6
Josue Martinez <joarmart@ucsc.edu>
2024-06-06 17:02:32 -0700

    Pipeline fixed and vendor app is back

--- VendorApp/test/views/Dashboard.test.tsx
+++ VendorApp/test/views/Dashboard.test.tsx
@@ -15,41 +15,37 @@ afterAll(() => server.close());
 
 const mockSetAccessToken = jest.fn();
 const mockSetUserName = jest.fn();
-const mockSetUserId = jest.fn();
 const mocksetView = jest.fn();
 
 const mockLoginContext = {
   userName: 'abcd',
   accessToken: '1234',
-  userId: '5678',
   setAccessToken: mockSetAccessToken,
   setUserName: mockSetUserName,
-  setUserId: mockSetUserId,
   view: 'Login',
   setView: mocksetView,
 };
 
 it('Renders Dashboard', () => {
-    render(<Dashboard/>)
+  render(<Dashboard/>)
 })
 
 it('clicks logout button', async () => {
-    global.fetch = jest.fn(() =>
-        Promise.resolve({
-          json: () => Promise.resolve({ errors: "N/A" }),
-        })
-    ) as jest.Mock;
-    window.alert = jest.fn()
-    render(
-      <LoginContext.Provider value={mockLoginContext}>
-        <Dashboard />
-      </LoginContext.Provider>
-    );
-    fireEvent.click(screen.getByText("Logout"))
-    await waitFor(() => {
-      expect(mockSetAccessToken).toHaveBeenCalledWith('');
-      expect(mockSetUserName).toHaveBeenCalledWith('');
-      expect(mockSetUserId).toHaveBeenCalledWith('');
-      expect(mocksetView).toHaveBeenCalledWith('Login');
-    });
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      json: () => Promise.resolve({ errors: "N/A" }),
+    })
+  ) as jest.Mock;
+  window.alert = jest.fn()
+  render(
+    <LoginContext.Provider value={mockLoginContext}>
+      <Dashboard />
+    </LoginContext.Provider>
+  );
+  fireEvent.click(screen.getByText("Logout"))
+  await waitFor(() => {
+    expect(mockSetAccessToken).toHaveBeenCalledWith('');
+    expect(mockSetUserName).toHaveBeenCalledWith('');
+    expect(mocksetView).toHaveBeenCalledWith('Login');
+  });
 });
